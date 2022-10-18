67e1177716802e30e8492c71b9ba55f2d497cde6
ifyoma <ifeoma.nlem@gmail.com>
2022-10-18 14:38:04 +0000

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *print_alphabet - prints alphabet
+ * print_alphabet - prints alphabet
  *
  * Return: always 0;
  *
@@ -9,10 +9,10 @@
 void print_alphabet(void)
 {
 	char i;
-	
-	for (i= 'a'; i <= 'z'; i++)
+
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
-	_putchar('\n);
+	_putchar('\n');
 }
